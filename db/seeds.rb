af4a174319b81760bbee38a7cbf5d7ddc12cd20d
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-16 10:29:42 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bob = Author.create(name: "Bob")
+steve = Author.create(name: "Steve")
+
+book1 = Book.create!(title: "Interesting Topic", pages: 40, publication_year: "2008")
+book2 = Book.create!(title: "Moderately Engaging Topic", pages: 80, publication_year: "2000")
+book3 = Book.create!(title: "Terrible Topic", pages: 2000, publication_year: "1950")
+
+bob.books << book1
+bob.books << book3
+steve.books << book2
+steve.books << book3
